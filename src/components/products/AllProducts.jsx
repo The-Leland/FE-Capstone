a3533d393d63e8875cf9b68f6745b4db5364c93a
Leland Lobato <L27Felix@hotmail.com>
2025-04-19 17:47:47 -0600

    trying something, thought I should save first

--- src/components/products/AllProducts.jsx
+++ src/components/products/AllProducts.jsx
@@ -1,49 +1,54 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import Header from "../navigation/Header";
+import Footer from "../navigation/Footer";
+import Sidebar from "../navigation/Sidebar";
 import FilterSection from "./product page components/ProductFilter";
 import SortProduct from "./product page components/SortProduct";
 import ProductCard from "./product page components/ProductCard";
 import { ShopContext } from "../context/ShopContext";
+import "../../styles/products/all-products.scss";
 
-function ProductPage() {
+const AllProducts = () => {
   const [sortField, setSortField] = useState('id');
   const [sortOrder, setSortOrder] = useState('asc');
-  const {products} = useContext(ShopContext);
+  const { products } = useContext(ShopContext);
   const [filteredProducts, setFilteredProducts] = useState([]);
-  
 
-useEffect(() => {
-  setFilteredProducts(products)
-}, [products])
-
-useEffect(() => {
-  console.log(filteredProducts)
-}, [filteredProducts])
+  useEffect(() => {
+    setFilteredProducts(products);
+  }, [products]);
 
   return (
-    <div>
-      <h2>Products</h2>
+    <div className="all-products-wrapper">
+      <Header />
+      <div className="all-products-page">
+        <Sidebar />
+        <div className="all-products-content">
+          <h2>All Products</h2>
 
-      <FilterSection
-        categories={["men's clothing", "jewelry", "electronics", "women's clothing"]}
-        filteredProducts={filteredProducts}
-        setFilteredProducts={setFilteredProducts}
-      />
+          <FilterSection
+            categories={["men's clothing", "jewelry", "electronics", "women's clothing"]}
+            filteredProducts={filteredProducts}
+            setFilteredProducts={setFilteredProducts}
+          />
 
-      
-      <SortProduct
-        sortField={sortField}
-        setSortField={setSortField}
-        sortOrder={sortOrder}
-        setSortOrder={setSortOrder}
-      />
+          <SortProduct
+            sortField={sortField}
+            setSortField={setSortField}
+            sortOrder={sortOrder}
+            setSortOrder={setSortOrder}
+          />
 
-      <div className="product-grid">
-        {filteredProducts.map(product => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+          <div className="product-grid">
+            {filteredProducts.map(product => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
-}
+};
 
-export default ProductPage;
+export default AllProducts;
