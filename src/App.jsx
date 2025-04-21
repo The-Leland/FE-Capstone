019f69dadf4f6bc2168704dd544aa95f5ae07999
Leland Lobato <L27Felix@hotmail.com>
2025-04-20 23:55:13 -0600

    real final push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; // React Router v5 imports
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import { useState } from 'react';
 
 import Header from './components/navigation/Header.jsx';
